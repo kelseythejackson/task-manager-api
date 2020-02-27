aef62edafc426e359858cbad28c0929239e5a208
Kelsey Jackson <kelseythej@gmail.com>
2020-02-26 18:45:07 -0600

    adds post route

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose')
-const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
   useNewUrlParser: true,
@@ -7,65 +6,18 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
   useUnifiedTopology: true
 })
 
-const User = mongoose.model('User', {
-  name: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  email: {
-    type: String,
-    required: true,
-    trim: true,
-    lowercase: true,
-    validate(value) {
-      if (!validator.isEmail(value)) {
-        throw new Error('Email is invalid')
-      }
-    }
-  },
-  age: {
-    type: Number,
-    default: 0,
-    validate(value) {
-      if (value < 0) {
-        throw new Error('Age must be a positive number')
-      }
-    }
-  }
-})
 
-const me = new User({
-  name: '    Kelsey    ',
-  email: 'lamar@jackson.com'
-})
-
-me.save().then(() => {
-  console.log(me)
-}).catch((error) => {
-  console.log('Error!', error.message)
-})
-
-// Define the Task Model
 
 const Task = mongoose.model('Task', {
   // model definitions
   description: {
-    type: String
+    type: String,
+    trim: true,
+    required: true
   },
   completed: {
-    type: Boolean
+    type: Boolean, 
+    default: false
   }
 })
 
-// an Instance of the Task Model
-// const task = new Task({
-//   description: 'Clean Apartment',
-//   completed: true
-// })
-
-// task.save().then(() => {
-//   console.log(task)
-// }).catch((error) => {
-//   console.log('Error!', error)
-// })
