4e25948faa4f35d168c271e091367edc31f2e1a5
Kelsey Jackson <kelseythej@gmail.com>
2020-03-03 21:26:37 -0600

    adds routes to read users

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -8,16 +8,5 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 
 
 
-const Task = mongoose.model('Task', {
-  // model definitions
-  description: {
-    type: String,
-    trim: true,
-    required: true
-  },
-  completed: {
-    type: Boolean, 
-    default: false
-  }
-})
+
 
