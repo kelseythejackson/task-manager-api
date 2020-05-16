737bb04ce5aa3dc89445897bae1591323439154d
Kelsey Jackson <kelseythej@gmail.com>
2020-05-16 11:17:27 -0500

    adds authenticaton to upload and delete routes for avatar

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const multer = require('multer')
 const upload = multer({
-  dest: 'avatars',
   limits: {
     fileSize: 1000000
   },
@@ -82,10 +81,19 @@ router.get('/me', auth, async (req, res) => {
   res.send(req.user)
 })
 
-router.post('/me/avatar', upload.single('avatar'), async (req, res) => {
+router.post('/me/avatar', auth, upload.single('avatar'), async (req, res) => {
+  req.user.avatar = req.file.buffer
+  await req.user.save()
   res.send()
+}, (error, req, res, next) => {
+  res.status(400).send({ error: error.message })
 })
 
+router.delete('/me/avatar', auth, async (req, res) => {
+  req.user.avatar = undefined
+  await req.user.save()
+  res.send()
+})
 
 
 router.patch('/me', auth, async (req, res) => {
